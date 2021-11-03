5b4e8bce278db54dacf724caeef8644b1d5654e7
MILK-HUB <87544320+MILK-HUB@users.noreply.github.com>
2021-11-02 19:52:08 -0500

    pls ya

---
+++ televisores/control.py
@@ -0,0 +1,35 @@
+from televisores.tv import *
+
+class Control:
+    
+    def enlazar(self, tv):
+        self.tv = tv
+        tv.control = self
+                    
+    def setCanal(self, canal):
+        self.tv.setCanal(canal)
+        
+    def getTv(self):
+        return self.tv
+    
+    def setTv(self, tv):
+        self.tv = tv
+    
+    def turnOn(self):
+        self.tv.estado = True
+            
+    def turnOff(self):
+        self.tv.estado = False
+    
+    def canalUp(self):
+        self.tv.canalUp()
+        
+    def canalDown(self):
+        self.tv.canalDown()
+                
+    def volumenUp(self):
+        self.tv.volumenUp()
+    
+    def volumenDown(self):
+        self.tv.volumenDown()
+   
